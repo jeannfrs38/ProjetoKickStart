c02bf364f4f9d04c5f8a5839b346e04d8f9f713e
jeannfrs <jeanfrs38@gmail.com>
2021-10-25 23:17:31 -0300

    Adicionando tela de tocar para começar o game e realizando algumas alteraçoes no labirinto

---
+++ Assets/Puzzle/Script/GameControle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControle : MonoBehaviour
+{
+    public Image panelStarted;
+    public bool gameStarted;
+    public float secondStart;
+
+    
+
+    void Start()
+    {
+        panelStarted = GetComponent<Image>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1){
+           StartCoroutine(GameStarted(secondStart));
+        }
+    }
+
+
+    IEnumerator GameStarted(float seconds){
+        yield return new WaitForSeconds (seconds);
+        gameStarted = true;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Puzzle/Script/Movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private GameControle _gameControle;
+     public Quaternion rotacao;
+    void Start()
+    {
+        _gameControle = FindObjectOfType(typeof(GameControle)) as GameControle;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_gameControle.gameStarted == true){
+             rotacao = transform.rotation;
+                
+            if(Input.touchCount > 0){
+               
+                Touch t = Input.GetTouch(0);
+               
+                if(t.phase == TouchPhase.Moved){
+                     
+                     float valueTy = t.deltaPosition.y;
+                     float valueTz = t.deltaPosition.x;
+                     float valueMenor = -4f;
+                     float valueMax = 4f;
+                     
+                    if(valueTz >= valueMax){
+                        valueTz = valueMax;
+                    }if(valueTz <= valueMenor){
+                        valueTz = valueMenor;
+                    }if(valueTy <= valueMenor){
+                        valueTy = valueMenor;
+
+                    }if(valueTy >= valueMax){
+                        valueTy = valueMax;
+                    }
+                    Vector3 rot = new Vector3(valueTy, 0.0f, valueTz * -1 );
+                    Debug.Log(rotacao.y);
+                 
+                    transform.Rotate( rot * 2 * Time.deltaTime, Space.World);
+                    
+                    
+
+                    
+                }
+            }else{
+                  if(rotacao.y >= 0 ){
+                        transform.rotation = Quaternion.Euler(transform.rotation.x, 0.0f, transform.rotation.z);
+                    }
+            }
+           
+        }
+       
+    }
+    
+}
